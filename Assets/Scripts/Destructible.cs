4a6ee897e08dd3a2e61742590cdca2d7daa574ef
n9tumi <mike.tune.mt1@gmail.com>
2021-10-19 11:24:41 +0300

    glass break improvements

--- Assets/Scripts/Destructible.cs
+++ Assets/Scripts/Destructible.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Destructible : MonoBehaviour
 {
+    [SerializeField]
+    Vector2 forceDirection;
+
+    [SerializeField]
+    float torque;
+
+    Rigidbody2D rb2d;
     // Start is called before the first frame update
     void Start()
     {
-        
+        float randTorque = UnityEngine.Random.Range(-200, 200);
+        float randForceX = UnityEngine.Random.Range(forceDirection.x - 50, forceDirection.x + 50);
+        float randForceY = UnityEngine.Random.Range(forceDirection.y, forceDirection.y + 50);
+
+        forceDirection.x = randForceX;
+        forceDirection.y = randForceY;
+
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(forceDirection);
+        rb2d.AddTorque(randTorque);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
