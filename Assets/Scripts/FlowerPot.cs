37457a1e5c53575646e9d064dc043684a3cc0dc9
n9tumi <mike.tune.mt1@gmail.com>
2021-10-21 18:23:10 +0300

    pot

---
+++ Assets/Scripts/FlowerPot.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerPot : MonoBehaviour
+{
+    private Animator m_Anim;
+    int hit = 0;
+
+
+
+    void Start()
+    {
+        m_Anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            hit++;
+            m_Anim.SetTrigger("Hit");
+
+            if(hit == 2)
+            {
+                GetComponent<BoxCollider2D>().enabled = false;
+            }
+        }
+    }
+}
