598e8742c4ffc5ee6c3b1999caeae4f428506e99
n9tumi <mike.tune.mt1@gmail.com>
2021-11-01 13:56:07 +0200

    sofa anim

---
+++ Assets/Scripts/AssetScripts/Sofa.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sofa : MonoBehaviour
+{
+    private Animator m_Anim;
+    private BoxCollider2D Ltrigger;
+    private BoxCollider2D Rtrigger;
+    private BoxCollider2D Mtrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Anim = GetComponent<Animator>();
+        var colliders = GetComponents<BoxCollider2D>();
+        Ltrigger = colliders[0];
+        Mtrigger = colliders[1];
+        Rtrigger = colliders[2];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (collision.IsTouching(Mtrigger))
+            {
+                m_Anim.SetTrigger("Hit");
+            }
+            if (collision.IsTouching(Ltrigger))
+            {
+                m_Anim.SetTrigger("LHit");
+            }
+            if (collision.IsTouching(Rtrigger))
+            {
+                m_Anim.SetTrigger("RHit");
+            }
+        }
+        
+    }
+
+}
