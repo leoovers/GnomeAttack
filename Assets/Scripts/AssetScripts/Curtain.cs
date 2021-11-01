d22993b9a5f5d7bfe6fc3d65acc2bb372681fcdf
n9tumi <mike.tune.mt1@gmail.com>
2021-11-01 10:51:30 +0200

    animation control

---
+++ Assets/Scripts/AssetScripts/Curtain.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curtain : MonoBehaviour
+{
+    private Animator m_Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            m_Anim.SetTrigger("rHit");
+
+
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            m_Anim.SetTrigger("Hit");
+        }
+    }
+}
---
+++ Assets/Scripts/AssetScripts/Painting.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Painting : MonoBehaviour
+{
+    private Animator m_Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            m_Anim.SetTrigger("Hit");
+
+
+        }
+    }
+}
