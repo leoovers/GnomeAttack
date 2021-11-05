7870d7463a0888dc2319d3808e572da09b9b0cd7
leoovers <leo.oversti@inv.inv>
2021-11-05 11:03:00 +0200

    removed freelook camera slider functions

--- Assets/Scripts/Level/CameraFollow.cs
+++ Assets/Scripts/Level/CameraFollow.cs
@@ -9,30 +9,18 @@ public class CameraFollow : MonoBehaviour
     public Catapult_physics mainScript;
     public Transform followTransform;
     public GameObject goal;
-    public Slider moveCamera;
-    public float cameraXvalue;
-    public bool toggleFreeLook;
     public float xOffset;
     public float yOffset;
-    private Slider moveCameraSlider;
-    public float smoothTime = 0.01F;
-    private Vector3 velocity = Vector3.zero;
+    public float smoothTime;
     public bool shaking = false;
+
+    private Vector3 velocity = Vector3.zero;
 
     void Start()
     {
-        moveCameraSlider = moveCamera.GetComponent<Slider>();
-        moveCameraSlider.onValueChanged.AddListener(delegate { cameraUpdate(); });  // Add listener for the slider
-
-        toggleFreeLook = false;
         xOffset = 9;
         yOffset = 2.0f;
         smoothTime = 1.7f;
-    }
-
-    void cameraUpdate()
-    {
-        cameraXvalue = moveCameraSlider.value;  // Move camera along X axis when slider is moved
     }
 
     // Update is called once per frame
@@ -40,7 +28,6 @@ public class CameraFollow : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            // Camera.main.position = Vector3.SmoothDamp(this.position, goal.position, ref velocity, smoothTime);
             followTransform = goal.transform;
         }
         if (Input.GetKeyUp(KeyCode.F))
@@ -51,24 +38,16 @@ public class CameraFollow : MonoBehaviour
     
     void FixedUpdate()
     {   
-        if (!toggleFreeLook & !shaking)
+        if (!shaking)
         {
             moveCamera.gameObject.SetActive(false);
             this.transform.position = Vector3.SmoothDamp(this.transform.position, new Vector3(followTransform.position.x + xOffset,
             followTransform.position.y + yOffset, -10), ref velocity, smoothTime);
         }
-        else if (toggleFreeLook)
-        {
-            moveCamera.gameObject.SetActive(true);
-            this.transform.position = new Vector3(cameraXvalue,
-            followTransform.position.y + yOffset, this.transform.position.z);
-        }
-
         
-
         if (mainScript.launched)
         {
-            xOffset = 3;  // Center the camera a bit more on launch
+            xOffset = 3;  // Center the camera a bit on launch
             smoothTime = 0.3f;
         }
         else
@@ -85,7 +64,7 @@ public class CameraFollow : MonoBehaviour
     public IEnumerator Shake(float duration, float magnitude)
     {
         shaking = true;
-        Debug.Log("Shaking");
+        Debug.Log("Camera shaking");
         Vector3 originalPos = transform.position;
         
         float elapsedTime = 0f;
