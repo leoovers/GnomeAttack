ba2527fc10dc514feedb7e324477f24eda890eab
n9tumi <mike.tune.mt1@gmail.com>
2021-11-08 13:40:18 +0200

    some Bathroom asset work

---
+++ Assets/Scripts/AssetScripts/Bathtub.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bathtub : MonoBehaviour
+{
+    private Animator m_Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            m_Anim.SetTrigger("Hit");
+        }
+            
+    }
+}
