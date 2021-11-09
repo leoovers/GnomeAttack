d2ea9cfe7eabbc99a061c5766d707bd8ddde8a3b
Sami Suopanki <n9susa00@students.oamk.fi>
2021-11-09 11:53:58 +0200

    code snippets for level progression

--- Assets/Scripts/Menu/LevelProgression.cs
+++ Assets/Scripts/Menu/LevelProgression.cs
@@ -1,15 +1,23 @@
-/*using System.Collections;
-using System.Collections.Generic;
+using System.Collections;
+/*using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LevelProgression : MonoBehaviour
 {
 
     //private GameObject LevelButtonArr;
     public LevelSelect levelSel;
+    private string sceneName;
+    private int LevelNum;
     void Start()
     {
+
+        sceneName = SceneManager.GetActiveScene().name.Substring(6);
+        LevelNum = int.Parse(sceneName);
+
+
         levelSel = Camera.main.GetComponent<LevelSelect>();
         int levelReached = PlayerPrefs.GetInt("levelReached", 1); //level progression
 
