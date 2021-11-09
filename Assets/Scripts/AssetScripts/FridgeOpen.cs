1b7e6ff5d57137ab2528b796e10fdc4f3a49a548
n9tumi <mike.tune.mt1@gmail.com>
2021-11-09 16:21:17 +0200

    all fridge anims

--- Assets/Scripts/AssetScripts/FridgeOpen.cs
+++ Assets/Scripts/AssetScripts/FridgeOpen.cs
@@ -7,6 +7,12 @@ public class FridgeOpen : MonoBehaviour
     private Animator m_Anim;
     private BoxCollider2D FreezerTrigger;
     private BoxCollider2D FrigeTrigger;
+    private BoxCollider2D PhotoTrigger;
+    private BoxCollider2D JulgubbeTrigger;
+    private BoxCollider2D ArtTrigger;
+    private BoxCollider2D FTrigger;
+    private BoxCollider2D CalendarTrigger;
+    private BoxCollider2D PisaTrigger;
     public Catapult_physics mainScript;
     int Hitcake = 0;
     // Start is called before the first frame update
@@ -15,8 +21,15 @@ public class FridgeOpen : MonoBehaviour
 
         m_Anim = GetComponent<Animator>();
         var colliders = GetComponents<BoxCollider2D>();
-        FreezerTrigger = colliders[0];
+        FreezerTrigger = colliders[3];
         FrigeTrigger = colliders[2];
+        PhotoTrigger = colliders[5];
+        FTrigger = colliders[4];
+        ArtTrigger = colliders[9];
+        JulgubbeTrigger = colliders[8];
+        CalendarTrigger = colliders[7];
+        PisaTrigger = colliders[6];
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -32,13 +45,37 @@ public class FridgeOpen : MonoBehaviour
             {
                 m_Anim.SetTrigger("Freezer");
             }
+            if (collision.IsTouching(PhotoTrigger))
+            {
+                m_Anim.SetTrigger("Photo");
+            }
+            if (collision.IsTouching(FTrigger))
+            {
+                m_Anim.SetTrigger("F");
+            }
+            if (collision.IsTouching(ArtTrigger))
+            {
+                m_Anim.SetTrigger("Art");
+            }
+            if (collision.IsTouching(JulgubbeTrigger))
+            {
+                m_Anim.SetTrigger("Julgubbe");
+            }
+            if (collision.IsTouching(CalendarTrigger))
+            {
+                m_Anim.SetTrigger("Calendar");
+            }
+            if (collision.IsTouching(PisaTrigger))
+            {
+                m_Anim.SetTrigger("Pisa");
+            }
 
 
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.CompareTag("Player"))
+        if (collision.collider.CompareTag("Player") & m_Anim.name == ("fridfecake"))
         {
             Hitcake++;
             m_Anim.SetTrigger("HitCake");
