cc52e0cb85dec46b832f4a4d71769b58180acbb1
Miikka <n9tumi00@students.oamk.fi>
2021-11-18 10:59:10 +0200

    BeeKill script

---
+++ Assets/Scripts/AssetScripts/BeeDestroy.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeDestroy : MonoBehaviour
+{
+    private GameObject grandparent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grandparent = this.transform.parent.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            grandparent.AddComponent<Rigidbody2D>();
+            this.Invoke("killbee", 3f);
+            
+        }
+        if(collision.collider.CompareTag("Ground") || collision.collider.CompareTag("Stickable"))
+        {
+            Destroy(grandparent);
+        }
+
+    }
+
+ 
+        
+        
+
+
+
+}
