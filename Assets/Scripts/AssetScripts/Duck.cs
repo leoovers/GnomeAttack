371aa219cbbc996fe64bcf7862007ba0f5f8e18d
n9tumi <mike.tune.mt1@gmail.com>
2021-11-22 21:29:00 +0200

    ducks and toilet work

---
+++ Assets/Scripts/AssetScripts/Duck.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duck : MonoBehaviour
+{
+    public Catapult_physics mainScript;
+    private Animator m_Anim;
+    private BoxCollider2D body;
+    private BoxCollider2D head;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Anim = GetComponent<Animator>();
+        var colliders = GetComponents<BoxCollider2D>();
+        body = colliders[0];
+        head = colliders[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            if (collision.collider.IsTouching(body))
+            {
+                m_Anim.SetTrigger("Hit");
+                body.enabled = false;
+
+            }
+            if (collision.collider.IsTouching(head))
+            {
+                m_Anim.SetTrigger("HatHit");
+                head.enabled = false;
+
+            }
+        }
+    }
+}
