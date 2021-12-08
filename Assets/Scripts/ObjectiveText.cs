d841ee42fb3a2f003c7abeaa2f05d7297f101135
Isabella Vicht <k1viis00@students.oamk.fi>
2021-12-08 12:51:40 +0200

    objective Text translated to german and english updated

--- Assets/Scripts/ObjectiveText.cs
+++ Assets/Scripts/ObjectiveText.cs
@@ -8,6 +8,11 @@ public class ObjectiveText : MonoBehaviour
 {
     public Catapult_physics mainScript;
     private Text objectiveText;
+    private string[] ger = new string[] { "Zerstöre den Zaun!", "Zerstöre die Blumen!", "Wirf den Griller um!", "Zerstöre das Bienennest!", "Erwische die Frösche!", "Setz den Garten unter Wasser!", "Zerstör das Fenster!", "Kletter zum Fenster rauf!",
+        "Setz die Küche unter Wasser!", "Zerbrich die Gläser!", "Zerbrich das Honigglas", "Öffne den Kühlschrank", "Zerstör den Kuchen!", "Wirf den Zucker um!", "Vermassle die Suppe!", "Erreiche die Türschnalle",
+        "Zerstör den Fernseher!", "Zebrich den Blumentopf", "Wirf die Uhr runter!", "Wirf das Gemälde runter", "Zieh die Vorhänge runter!", "Zerstör die Vitrine", "Spring auf die Couch und zerstöre die Lampen!", "Kletter die Stufen rauf!",
+        "Zerstör die Tür!", "Verstopfe die Toilette", "Lass die Seifen runter schlittern!", "Schalte die Waschmaschine an!", "Mach ein Schaumbad!", "Erwische die Enten!", "Wirf das Handtuch runter!", "Flieg durch die Tür!",
+        "Zerstör die Tür!", "Spring auf das Bett und zerstöre die Lampen!", "Schlat das Radio an!", "Öffne den Kasten!", "Wirf das Spielzeug runter!", "Zerstör den Polster!", "Wirf die Bücher um!", "Flieg durchs Fenster!"};
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +28,7 @@ public class ObjectiveText : MonoBehaviour
 
         setObjText("Level_9", "Flood the kitchen!");
         setObjText("Level_10", "Break the glass!");
-        setObjText("Level_11", "Break the vase!");
+        setObjText("Level_11", "Break the honey jar!");
         setObjText("Level_12", "Open the fridge!");
         setObjText("Level_13", "Destroy the cake!");
         setObjText("Level_14", "Tip over the sugar bag!");
@@ -42,22 +47,20 @@ public class ObjectiveText : MonoBehaviour
         setObjText("Level_25", "Break the door!");
         setObjText("Level_26", "Clog the toilet!");
         setObjText("Level_27", "Slide the soaps!");
-        setObjText("Level_28", "Flood the tub!");
+        setObjText("Level_28", "Turn on the washing machine!");
         setObjText("Level_29", "Soap the bath!");
         setObjText("Level_30", "Hit the ducks!");
         setObjText("Level_31", "Drop the towel!");
         setObjText("Level_32", "Fly through the door!");
 
-        setObjText("Level_33", "NO LEVEL!");
-
-        setObjText("Level_34", "Destroy the door!");
-        setObjText("Level_35", "Destroy the lamp!");
-        setObjText("Level_36", "Turn on the radio!");
-        setObjText("Level_37", "Open the cupboards!");
-        setObjText("Level_38", "Throw down the toys!");
-        setObjText("Level_39", "Destroy the pillow!");
-        setObjText("Level_40", "Hit the books!");
-        setObjText("Level_41", "Fly through the window!");
+        setObjText("Level_33", "Destroy the door!");
+        setObjText("Level_34", "Bounce on the bed and destroy the lamp!");
+        setObjText("Level_35", "Turn on the radio!");
+        setObjText("Level_36", "Open the cupboards!");
+        setObjText("Level_37", "Throw down the toys!");
+        setObjText("Level_38", "Destroy the pillow!");
+        setObjText("Level_39", "Hit the books!");
+        setObjText("Level_40", "Fly through the window!");
 
     }
 
