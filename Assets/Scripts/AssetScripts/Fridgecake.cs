5bf7ae43eeb17010934da1ee584a29be2fce2a80
n9tumi <mike.tune.mt1@gmail.com>
2021-12-16 13:13:40 +0200

    fridge changes

--- Assets/Scripts/AssetScripts/Fridgecake.cs
+++ Assets/Scripts/AssetScripts/Fridgecake.cs
@@ -8,11 +8,28 @@ public class Fridgecake : MonoBehaviour
     private Animator m_Anim;
     private int Hitcake;
     public Catapult_physics mainScript;
+    private BoxCollider2D FreezerTrigger;
+    private BoxCollider2D Cake1;
+    private BoxCollider2D RBox;
+    private BoxCollider2D Cake2;
+    private BoxCollider2D LBox;
+    private BoxCollider2D Cheese;
+    private BoxCollider2D Pudding;
+    private BoxCollider2D Photo;
     // Start is called before the first frame update
     void Start()
     {
         m_Anim = GetComponent<Animator>();
-        Hitcake = 0;   
+        Hitcake = 0;
+        var colliders = GetComponents<BoxCollider2D>();
+        FreezerTrigger = colliders[0];
+        Cake1 = colliders[1];
+        RBox = colliders[2];
+        Cake2 = colliders[3];
+        LBox = colliders[6];
+        Cheese = colliders[5];
+        Pudding = colliders[4];
+        Photo = colliders[7];
     }
 
     // Update is called once per frame
@@ -24,9 +41,22 @@ public class Fridgecake : MonoBehaviour
     {
         if (collision.collider.CompareTag("Player"))
         {
-            Hitcake++;
-            m_Anim.SetTrigger("HitCake");
-            GetComponent<CapsuleCollider2D>().enabled = false;
+            if (collision.collider.IsTouching(Cake1) || collision.collider.IsTouching(Cake2))
+            {
+                Hitcake++;
+                m_Anim.SetTrigger("HitCake");
+                Cake1.enabled = false;
+                this.Invoke("cake", 0.3f);
+            }
+            if (collision.collider.IsTouching(RBox))
+            {
+                m_Anim.SetTrigger("RBox");
+            }
+            if (collision.collider.IsTouching(FreezerTrigger))
+            {
+                m_Anim.SetTrigger("Freezer");
+            }
+
 
             if (Hitcake >= 2)
             {
@@ -35,5 +65,35 @@ public class Fridgecake : MonoBehaviour
             }
 
         }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (collision.IsTouching(LBox))
+            {
+                m_Anim.SetTrigger("LBox");
+
+            }
+            if (collision.IsTouching(Cheese))
+            {
+                m_Anim.SetTrigger("Cheese");
+
+            }
+            if (collision.IsTouching(Pudding))
+            {
+                m_Anim.SetTrigger("Pudding");
+
+            }
+            if (collision.IsTouching(Photo))
+            {
+                m_Anim.SetTrigger("Photo");
+
+            }
+        }
+    }
+    private void cake()
+    {
+        Cake2.enabled = true;
     }
 }
